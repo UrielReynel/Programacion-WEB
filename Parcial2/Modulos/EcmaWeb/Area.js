9ff7afb3624ca97592e135f7fbd4f746c308fd3b
Uriel Reynel <urielreynel194@gmail.com>
2023-03-15 07:17:35 -0500

    Actualizacion modulos

---
+++ Parcial2/Modulos/EcmaWeb/Area.js
@@ -0,0 +1,12 @@
+export function areaCuadrado(lado){
+    return lado * lado;
+}
+
+export function areaTriangulo(base,altura){
+    return (base*altura)/2;
+}
+
+// module.exports.areaCuadrado = areaCuadrado;
+// module.exports.areaTriangulo = areaTriangulo;
+
+//console.log(module);
